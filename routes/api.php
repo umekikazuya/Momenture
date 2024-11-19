1b7e1f6bf3d582a23e20480266d7c266ce21a606
Kazuya Umeki <kazuyaumeki1115@gmail.com>
2024-11-19 14:33:32 +0900

    Fix

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-Route::prefix('profile')->group(function () {
-    Route::get('/', [ProfileController::class, 'show']);
-    Route::post('/', [ProfileController::class, 'store']);
-    Route::put('/', [ProfileController::class, 'update']);
-    Route::delete('/', [ProfileController::class, 'destroy']);
-});
