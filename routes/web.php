9436fc981ca9c616cb493a00e7f32b987bd4a9e0
Kazuya Umeki <kazuyaumeki1115@gmail.com>
2024-11-19 13:58:52 +0900

    Update vercel.json

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('profile')->group(function () {
+    Route::get('/', [ProfileController::class, 'show']);
+    Route::post('/', [ProfileController::class, 'store']);
+    Route::put('/', [ProfileController::class, 'update']);
+    Route::delete('/', [ProfileController::class, 'destroy']);
 });
