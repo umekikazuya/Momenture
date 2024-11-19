63cb9e705597c43449560d1445800a85b31015bd
Kazuya Umeki <kazuyaumeki1115@gmail.com>
2024-11-19 14:40:43 +0900

    Fix

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::prefix('profile')->group(function () {
-    Route::get('/', [ProfileController::class, 'show']);
-    Route::post('/', [ProfileController::class, 'store']);
-    Route::put('/', [ProfileController::class, 'update']);
-    Route::delete('/', [ProfileController::class, 'destroy']);
 });
