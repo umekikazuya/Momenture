8c766da22055ee6de7cf1578bde02e739bcf4e28
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2025-03-24 04:13:48 +0900

    Fix

--- app/Http/Middleware/BasicAuthMiddleware.php
+++ app/Http/Middleware/BasicAuthMiddleware.php
@@ -84,6 +84,5 @@ class BasicAuthMiddleware
             ->header('Cache-Control', 'no-cache, no-store, must-revalidate')
             ->header('Pragma', 'no-cache')
             ->header('Expires', '0');
-
     }
 }
