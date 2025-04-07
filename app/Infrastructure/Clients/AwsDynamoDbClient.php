c1ffa7bbbb04e383d6b069c5901e2b456808198d
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2025-04-07 18:35:28 +0900

    fix

--- app/Infrastructure/Clients/AwsDynamoDbClient.php
+++ app/Infrastructure/Clients/AwsDynamoDbClient.php
@@ -10,6 +10,15 @@ class AwsDynamoDbClient implements DynamoDbClientInterface
     public function __construct(
         private readonly DynamoDbClient $client
     ) {
+        $this->client = new DynamoDbClient([
+            'region' => config('database.connections.dynamodb.region'),
+            'version' => config('database.connections.dynamodb.version'),
+            'endpoint' => config('database.connections.dynamodb.endpoint'),
+            'credentials' => [
+                'key' => config('database.connections.dynamodb.key'),
+                'secret' => config('database.connections.dynamodb.secret'),
+            ],
+        ]);
     }
 
     public function getItem(array $args): Result
